303ec8561546074f90f499c6f668f49d5e94471e
gasiorradek@gmail.com <gasiorradek@gmail.com>
2019-09-22 12:44:19 +0200

    pick function

---
+++ exercises/only.js
@@ -0,0 +1,43 @@
+function onlyVersion1(object, propertyNames) {
+    const entries = Object.entries(object);
+    const result = {};
+
+    Object.entries(object).forEach(([propertyName, propertyValue]) => {
+        if (propertyNames.includes(propertyName)) {
+            result[propertyName] = propertyValue;
+        }
+    });
+    return result;
+};
+function onlyVersion2(object, propertyNames) {
+    const result = {};
+
+    propertyNames.forEach((propertyName) => {
+       if (Object.keys(object).includes(propertyName)){
+            result[propertyName] = object[propertyName];
+       }
+    });
+
+    return result;
+
+};
+
+const numbers = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+}
+
+console.log(
+    'onlyVersion2(numbers, ["b", "c", "k"])',
+    onlyVersion2(numbers, ["b", "c", "k"])
+);
+
+console.log('Object.values(numbers)', Object.values(numbers));
+console.log('Object.keys(numbers)', Object.keys(numbers));
+console.log('Object.entries(numbers)', Object.entries(numbers));
+const result = ['foo', 'bar', 'baz']
+    .map((item) => `${item}!`)
+    .map((item) => `${item}!!`);
+console.log('result', result);
